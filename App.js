6e7b4b3b624d4f306fe0be097870132e6fe97a72
Max <erin@Karlas-MacBook-Pro.local>
2023-01-17 18:04:22 -0800

    adds Header component and passes app name as props

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
+  const name = "my awesome app";
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        Welcome to {name}</Text>
       <StatusBar style="auto" />
+      <Header name = "Max" />
     </View>
   );
 }
